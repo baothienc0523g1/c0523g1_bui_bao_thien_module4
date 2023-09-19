984ae29ece704a0bd1ffd805ea7907297dbccead
baothienc0523g1 <noimandembuongxuong@gmail.com>
2023-09-19 21:32:51 +0700

    practice

--- SS05/practice/demo/src/main/java/com/codegym/service/ICustomerService.java
+++ SS05/practice/demo/src/main/java/com/codegym/service/ICustomerService.java
@@ -1,2 +1,15 @@
-package com.codegym.service;public interface ICustomerService {
+package com.codegym.service;
+
+import com.codegym.model.Customer;
+
+import java.util.List;
+
+public interface ICustomerService {
+    List<Customer> findAll();
+
+    void save(Customer customer);
+
+    Customer findById(int id);
+
+    void remove(int id);
 }
